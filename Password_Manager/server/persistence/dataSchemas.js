c499d4e92a88dc4e5fe9b9197516ff11ffce90f2
Oliver-IV <145882378+Oliver-IV@users.noreply.github.com>
2024-08-03 22:58:44 -0700

    Little fix on validations with ZOD

--- Password_Manager/server/persistence/dataSchemas.js
+++ Password_Manager/server/persistence/dataSchemas.js
@@ -1,9 +1,9 @@
 import { z } from "zod" ;
 
 const userSchema = z.object({
-    names: z.string().max(30).regex(/^[a-zA-Z\s]+$/, { message: 'Only letters allowed' }).min(1),
-    last_name_p: z.string().max(30).regex(/^[a-zA-Z\s]+$/, { message: 'Only letters allowed' }).min(1),
-    last_name_m: z.string().max(30).regex(/^[a-zA-Z\s]+$/, { message: 'Only letters allowed' }).min(1),  
+    names: z.string().max(30).regex(/^[\p{L}\s]+$/u, { message: 'Only letters allowed' }).min(1),
+    last_name_p: z.string().max(30).regex(/^[\p{L}\s]+$/u, { message: 'Only letters allowed' }).min(1),
+    last_name_m: z.string().max(30).regex(/^[\p{L}\s]+$/u, { message: 'Only letters allowed' }).min(1),  
     email: z.string().email().min(1),
     password: z.string().min(8).regex(/^(.*\d){3}/, { message: 'Password must contain at least 3 numbers' }).min(1),
   });
@@ -14,7 +14,7 @@ const userSchema = z.object({
   });
   
   const accountSchema = z.object({
-    name: z.string().min(1).max(18).regex(/^[a-zA-Z0-9\s]+$/, { message: 'Name must contain only letters, numbers' }),
+    name: z.string().min(1).max(18).regex(/^[\p{L}\p{N}\s]+$/u, { message: 'Name must contain only letters, numbers' }),
     user: z.string().min(1).max(30),
     password: z.string().min(1).max(25),
   });
